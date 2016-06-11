8e8328299de9cb31dcc59e38b19f2de3f638a6f5
Damian Ficner <maniek1124@gmail.com>
2016-06-12 01:18:01 +0200

    GUI
    Poprawienie wyglądu projektu poprzez CSS

--- main.php
+++ main.php
@@ -4,14 +4,17 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
 
 <head>
+<link rel="stylesheet" type="text/css" href="css.css">
 <meta http-equiv="content-type" content="text/html; charset=utf-8">
 <script type="text/javascript" src="skrypt.js"></script>
 </head>
 
-<body leftmargin="200" rightmargin="200" marginwidth="200">
-    <input type="button" value="NEW" onclick="nowy();" />
-    <input type="button" value="Generate iCal" onclick="generuj();" /> <br /> <br />
-    Adres URL:<input type="text" id="url" />
+
+<body>
+<div class="cialo">
+    <input type="button" value="NEW" id = "new" onclick="nowy();"  />
+    <input type="button" id="generate" value="Generate iCal" onclick="generuj();" /> <br /> <br />
+    <img src="icon_url.png" align="top">    <input type="text" id="url" style="background-color: #C0C0C0;" />
     Data:
     <?php
 echo '<select id="dzien">';
@@ -31,10 +34,12 @@ for ($year = 2016; $year <= 2050; $year++)
 echo "<option> $year </option>";
 echo '</select>'; 
 ?>
+<br/> <br/>
     <div id="pole" width = '400px' height ='200px'> </div>
-    <input type="button" value="Reset" onclick="resetuj();"/> 
-    <input type="button" value="Download iCal" onclick="pobierz();" />
+    <input type="button" value="Reset" id="reset" onclick="resetuj();"/> 
+    <input type="button" value="Download iCal" id="download" onclick="pobierz();" />
     <div id="informacja" />
     <div id="kod" />
+	</div>
 </body>
 </html>
